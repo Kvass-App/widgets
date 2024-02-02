242961933eddd56fdbe5dc9deef033dca4f895ab
Sigve Hansen <sighan12@gmail.com>
2024-02-02 15:16:10 +0100

    fix(flatfinder & map): add lazy loading to components

--- src/flatfinder/components/Flatfinder.ce.vue
+++ src/flatfinder/components/Flatfinder.ce.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Flatfinder as FlatfinderComponent } from '@kvass/vue3-flatfinder'
+import { LazyLoad } from '@kvass/ui'
 
 const props = defineProps({
   projectUrl: {
@@ -18,12 +19,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <FlatfinderComponent
-    class="kvass-flatfinder"
-    :project-url="projectUrl"
-    :project-id="projectId"
-    :path-prefix="pathPrefix"
-  />
+  <LazyLoad>
+    <FlatfinderComponent
+      class="kvass-flatfinder"
+      :project-url="projectUrl"
+      :project-id="projectId"
+      :path-prefix="pathPrefix"
+    />
+  </LazyLoad>
 </template>
 
 <style>
