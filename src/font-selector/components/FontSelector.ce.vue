c8f8d9195a50157cd835f2411497b73f963d8834
Sigve Hansen <sighan12@gmail.com>
2024-02-07 13:42:19 +0100

    fix: implement font preview

--- src/font-selector/components/FontSelector.ce.vue
+++ src/font-selector/components/FontSelector.ce.vue
@@ -1,96 +1,224 @@
-<script lang="ts" setup>
-import { useMachine, normalizeProps } from '@zag-js/vue'
-import * as select from '@zag-js/select'
-import { computed } from 'vue'
-import { useCurrentElement } from '@vueuse/core'
+<script setup>
+import '@kvass/ui/style.css'
+import { computed, onMounted, ref } from 'vue'
+import { providers } from '../providers.js'
+import WebFontLoader from 'webfontloader'
 
-const fonts = [
-  {
-    value: 'Roboto',
-    label: 'Roboto',
+const props = defineProps({
+  placeholder: String,
+  provider: {
+    type: String,
+    default: 'google',
   },
-  {
-    value: 'Open+Sans',
-    label: 'Open Sans',
+  type: {
+    type: String,
+    default: 'text',
+    validator: (value) => ['text', 'heading'].includes(value),
   },
-  {
-    value: 'Lato',
-    label: 'Lato',
-  },
-  {
-    value: 'Montserrat',
-    label: 'Montserrat',
-  },
-  {
-    value: 'Roboto+Condensed',
-    label: 'Roboto Condensed',
-  },
-  {
-    value: 'Oswald',
-    label: 'Oswald',
-  },
-  {
-    value: 'Poppins',
-    label: 'Poppins',
-  },
-  {
-    value: 'Raleway',
-    label: 'Raleway',
-  },
-  {
-    value: 'Slabo+27px',
-    label: 'Slabo 27px',
-  },
-  {
-    value: 'Merriweather',
-    label: 'Merriweather',
-  },
-]
+  defaultFont: String,
+})
 
-const currentElement = useCurrentElement()
-
-const [state, send] = useMachine(
-  select.machine({
-    id: 'font-selector',
-    collection: select.collection({
-      items: fonts,
-      itemToString: (item) => item.label,
-    }),
-    getRootNode: currentElement.value?.getRootNode,
-  }),
+const selectedProvider = computed(() =>
+  providers.find((p) => p.value === props.provider),
+)
+const selectedFont = ref(
+  selectedProvider.value?.fonts.find((f) => f.value === props.defaultFont) ||
+    selectedProvider.value?.fonts[0],
 )
 
-const api = computed(() => select.connect(state.value, send, normalizeProps))
+const styles = computed(
+  () =>
+    selectedFont.value &&
+    `--kvass-font-selector-font-family: '${selectedFont.value.value}'`,
+)
+
+onMounted(() => {
+  // Initialize WebFontLoader
+  const config = Object.fromEntries(
+    Object.entries(providers).map(([_, provider]) => [
+      provider.value,
+      { families: provider.fonts.map((font) => `${font.value}:400,700`) },
+    ]),
+  )
+
+  WebFontLoader.load(config)
+})
 </script>
 
 <template>
-  <div class="kvass-font-selector">
-    <div>
-      <label>Teksttype</label>
-      <button v-bind="api.triggerProps">
-        <span>{{ api.valueAsString || 'Velg teksttype' }}</span>
-      </button>
-    </div>
-
-    <div v-bind="api.positionerProps">
-      <ul v-bind="api.contentProps">
-        <li
-          v-for="item in fonts"
-          :key="item.value"
-          v-bind="api.getItemProps({ item })"
-        >
-          <span>{{ item.label }}</span>
-          <span v-bind="api.getItemIndicatorProps({ item })">✓</span>
-        </li>
-      </ul>
-    </div>
-
-    <select v-bind="api.hiddenSelectProps">
-      <option
-        v-for="item in fonts"
-        :key="item.value"
-        v-bind="api.getItemProps({ item })"
-      ></option>
+  <div class="kvass-font-selector" :style="styles">
+    <select v-model="selectedFont" class="kvass-font-selector__input">
+      <optgroup
+        v-for="provider in providers"
+        :key="provider.value"
+        :label="provider.label"
+      >
+        <option v-for="font in provider.fonts" :key="font.value" :value="font">
+          {{ font.label }}
+        </option>
+      </optgroup>
     </select>
+    <div
+      :class="[
+        `kvass-font-selector__preview kvass-font-selector__preview--${props.type}`,
+      ]"
+    >
+      <small class="kvass-font-selector__preview-label">Forhåndsvisning</small>
+      <template v-if="props.type === 'heading'">
+        <h1>Hovedtittel</h1>
+        <h2>Mellomtittel</h2>
+        <h3>Tertiærtittel</h3>
+      </template>
+      <template v-if="props.type === 'text'">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          imperdiet, nulla et dictum interdum.
+        </p>
+      </template>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.kvass-font-selector {
+  // Default variables
+  --__kvass-font-selector-background-color: white;
+  --__kvass-font-selector-padding-y: 1rem;
+  --__kvass-font-selector-padding-x: 1rem;
+  --__kvass-font-selector-border-width: 1px;
+  --__kvass-font-selector-border-style: solid;
+  --__kvass-font-selector-border-color: #eaeaea;
+  --__kvass-font-selector-border-radius: 6px;
+  --__kvass-font-selector-max-width: 500px;
+
+  border-radius: var(
+    --kvass-font-selector-border-radius,
+    var(--__kvass-font-selector-border-radius)
+  );
+  background-color: var(
+    --kvass-font-selector-background-color,
+    var(--__kvass-font-selector-background-color)
+  );
+  max-width: var(
+    --kvass-font-selector-max-width,
+    var(--__kvass-font-selector-max-width)
+  );
+  position: relative;
+
+  &::after {
+    content: url('../selector.svg');
+    position: absolute;
+    right: var(
+      --kvass-font-selector-padding-x,
+      var(--__kvass-font-selector-padding-x)
+    );
+    top: var(
+      --kvass-font-selector-padding-y,
+      var(--__kvass-font-selector-padding-y)
+    );
+    pointer-events: none;
+    opacity: 0.5;
+  }
+
+  &__input {
+    appearance: none;
+    font: inherit;
+    width: 100%;
+    padding: var(
+        --kvass-font-selector-padding-y,
+        var(--__kvass-font-selector-padding-y)
+      )
+      var(
+        --kvass-font-selector-padding-x,
+        var(--__kvass-font-selector-padding-x)
+      );
+    border: var(
+        --kvass-font-selector-border-width,
+        var(--__kvass-font-selector-border-width)
+      )
+      var(
+        --kvass-font-selector-border-style,
+        var(--__kvass-font-selector-border-style)
+      )
+      var(
+        --kvass-font-selector-border-color,
+        var(--__kvass-font-selector-border-color)
+      );
+    border-top-left-radius: var(
+      --kvass-font-selector-border-radius,
+      var(--__kvass-font-selector-border-radius)
+    );
+    border-top-right-radius: var(
+      --kvass-font-selector-border-radius,
+      var(--__kvass-font-selector-border-radius)
+    );
+    border-bottom: none;
+  }
+
+  &__preview {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: var(
+        --kvass-font-selector-padding-y,
+        var(--__kvass-font-selector-padding-y)
+      )
+      var(
+        --kvass-font-selector-padding-x,
+        var(--__kvass-font-selector-padding-x)
+      );
+    border: var(
+        --kvass-font-selector-border-width,
+        var(--__kvass-font-selector-border-width)
+      )
+      var(
+        --kvass-font-selector-border-style,
+        var(--__kvass-font-selector-border-style)
+      )
+      var(
+        --kvass-font-selector-border-color,
+        var(--__kvass-font-selector-border-color)
+      );
+    border-bottom-right-radius: var(
+      --kvass-font-selector-border-radius,
+      var(--__kvass-font-selector-border-radius)
+    );
+    border-bottom-left-radius: var(
+      --kvass-font-selector-border-radius,
+      var(--__kvass-font-selector-border-radius)
+    );
+
+    p,
+    h1,
+    h2,
+    h3 {
+      margin: 0;
+      font-family: var(--kvass-font-selector-font-family);
+      text-wrap: balance;
+    }
+
+    p {
+      line-height: 1.5;
+    }
+
+    h1,
+    h2,
+    h3 {
+      line-height: 1;
+    }
+
+    &--heading {
+      font-weight: 700;
+    }
+
+    &--text {
+      font-weight: 400;
+    }
+
+    &-label {
+      opacity: 0.5;
+      letter-spacing: 0.25px;
+    }
+  }
+}
+</style>
