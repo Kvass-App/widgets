dc900ed7beca450115d177221c9ca8659b7a28b8
Carsten Jacobsen <car.jacobsen@gmail.com>
2024-11-07 11:15:25 +0100

    fix(knips): drop iframe

--- src/knips/components/Knips.ce.vue
+++ src/knips/components/Knips.ce.vue
@@ -1,166 +1,329 @@
 <script setup>
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
+import { Card, Grid, Button, Dialog, Image, Icon, Flex } from '@kvass/ui'
+
+const Shorten = (e, l) => (e.length > l ? e.substring(e, l - 3) + '...' : e)
+const Replace = (s, d) => s.replace(/{(\w+)}/g, (m, k) => d[k] || m)
+const isUrl = (e) => e.match(/^https?:\/\/|\//)
+const FormatDate = (e) =>
+  new Intl.DateTimeFormat(navigator.language, {
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric',
+  }).format(new Date(e))
+const WrapMap = { bold: 'b', italic: 'i' }
+const Wrap = (t, e) => `<${WrapMap[t] || t}>${e}</${WrapMap[t] || t}>`
+const defaultUrl =
+  '/api/integration/preset/knips/statics/getPosts?feed={feed}&limit={limit}'
 
 const props = defineProps({
   url: String,
   feed: String,
-  shouldDisplayAuthor: {
+  author: {
     type: Boolean,
     default: true,
   },
-  postsLayout: {
-    type: String,
-    default: 'grid',
-  },
-  postDesign: {
-    type: String,
-    default: 'standard',
-  },
-  maxNumberOfPosts: {
+  posts: {
     type: Number,
-    default: 4,
+    default: 3,
   },
-  maxNumberOfColumns: {
+  columns: {
     type: Number,
-    default: 4,
+    default: 'repeat(auto-fit, minmax(300px, 1fr))',
   },
-  background: String,
-  primary: String,
-  primaryContrast: String,
-  surface: String,
-  surfaceContrast: String,
 })
-
-const hex = /#?[\da-f]{6}/
-const hsl = /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/i
-
-function hslToHex(hsl) {
-  // Extract the HSL values from the input string
-  const [_, h, s, l] =
-    hsl.match(/hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/i) || []
-  if (!h || !s || !l) return null
-
-  // Convert percentages to fractions
-  const hue = Number(h.match(hsl) ? getVariable(h) : h)
-  const saturation = Number(s.match(hsl) ? getVariable(s) : s) / 100
-  const lightness = Number(l.match(hsl) ? getVariable(l) : l) / 100
-
-  const c = (1 - Math.abs(2 * lightness - 1)) * saturation
-  const x = c * (1 - Math.abs(((hue / 60) % 2) - 1))
-  const m = lightness - c / 2
-
-  let r = 0,
-    g = 0,
-    b = 0
-
-  if (0 <= hue && hue < 60) {
-    r = c
-    g = x
-    b = 0
-  } else if (60 <= hue && hue < 120) {
-    r = x
-    g = c
-    b = 0
-  } else if (120 <= hue && hue < 180) {
-    r = 0
-    g = c
-    b = x
-  } else if (180 <= hue && hue < 240) {
-    r = 0
-    g = x
-    b = c
-  } else if (240 <= hue && hue < 300) {
-    r = x
-    g = 0
-    b = c
-  } else if (300 <= hue && hue < 360) {
-    r = c
-    g = 0
-    b = x
-  }
-
-  // Convert RGB to Hex and return the formatted string
-  const toHex = (n) => {
-    const hex = Math.round((n + m) * 255)
-      .toString(16)
-      .padStart(2, '0')
-    return hex
-  }
-
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}`
-}
-
-const getColor = (value) => {
-  let color = value?.match(hex) ? value : getVariable(value)
-
-  if (color.match(hsl)) color = hslToHex(color)
-  return color.startsWith('#') ? color.substring(1) : color
-}
-
-const getVariable = (variable) => {
-  if (!variable) return '#000000'
-  variable = variable.startsWith('var(--')
-    ? variable.substring(6, -1)
-    : variable
-
-  return getComputedStyle(
-    document.body.querySelector('.standard-project') || document.body,
-  ).getPropertyValue(`--${variable}`)
-}
 
 const el = ref(null)
-const src = computed(() => {
-  const url = new URL(`${props.url}/embed/posts`)
+const dialog = ref(null)
 
-  Object.entries(props).forEach(([prop, value]) => {
-    if (prop === 'url') return
+const items = ref([])
+const item = ref(null)
+const mapItem = (item) => {
+  return {
+    id: item.id,
+    title: Shorten(item.text, 110),
+    date: FormatDate(item.publishDate),
+    author: item?.author?.name,
+    thumbnail: item.featuredImage.src,
+    claps: item.clapCount,
+    content: [
+      ...item.blog
+        .flatMap((b) => {
+          switch (b.type) {
+            case 'image':
+              return
+            case 'h1':
+            case 'h2':
+              return { component: 'h2', content: b.text }
+            case 'form':
+              const form = item.forms.find((f) => f.id === b.formId)
+              return {
+                component: Button,
+                label: form.title,
+                variant: 'tertiary',
+                is: 'a',
+                href: `${props.url}/p/${item.id}?showForm=${form.id}`,
+                target: '_blank',
+              }
+            case 'link':
+              if (
+                b.url.replace(/\/$/, '') ===
+                (window.location.origin + window.location.pathname).replace(
+                  /\/$/,
+                  '',
+                )
+              )
+                return
+              return {
+                component: b.style === 'button' ? Button : 'a',
+                [b.style === 'button' ? 'label' : 'content']: b.text,
+                variant: 'tertiary',
+                is: 'a',
+                href: b.urlRedirect || b.url,
+                target: '_blank',
+              }
+            case 'regular':
+              if (!b.spans?.length) return { component: 'p', content: b.text }
+              return b.spans.map((s) => {
+                let content = s.text
+                s.styles.forEach((t) => (content = Wrap(t, content)))
+                return { component: 'p', content }
+              })
+          }
+        })
+        .filter(Boolean),
+    ],
+  }
+}
 
-    const map = {
-      feed: 'feedId',
-      surfaceContrast: 'onSurface',
-      primaryContrast: 'onPrimary',
-    }
+const clap = async (item) => {
+  fetch(
+    `/api/integration/preset/knips/statics/clap?feed=${props.feed}&post=${item.id}`,
+  )
+  item.claps = item.claps + 1
+}
 
-    const k = map[prop] || prop
+const getPosts = () => {
+  const { feed, posts: limit } = props
+  const api = Replace(isUrl(feed) ? feed : defaultUrl, { feed, limit })
 
-    switch (k) {
-      case 'background':
-      case 'primary':
-      case 'surface':
-      case 'onSurface':
-      case 'onPrimary':
-        url.searchParams.set(k, getColor(value))
-        break
-      default:
-        url.searchParams.set(k, value)
-        break
-    }
-  })
+  fetch(api)
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        (items.value = data
+          .filter((e) => e.status === 'published' && e.channel === 'public')
+          .map(mapItem)),
+    )
+  // .then(() => onDialog(items.value.at(1)))
+}
 
-  return url.toString()
-})
+const onDialog = (e) => {
+  item.value = e
+  if (e) dialog.value.open()
+}
 
-onMounted(() => {
-  window.addEventListener('message', (t) => {
-    if (!/"id":"nextjs"/.test(t.data)) return
-
-    el.value.style.height = JSON.parse(t.data).height + 'px'
-  })
-})
+onMounted(getPosts)
 </script>
 
 <template>
-  <iframe
-    ref="el"
-    allowfullscreen="true"
-    width="100%"
-    class="knips-feed"
-    :src="src"
-  ></iframe>
+  <div class="knips-feed">
+    <Grid class="knips-feed__items" :columns="columns">
+      <Card v-for="item in items" :key="item.id">
+        <template #header>
+          <Image
+            :src="item.thumbnail"
+            aspect-ratio="16/9"
+            class="k-card__thumbnail"
+          />
+        </template>
+        <template #default>
+          <Flex data-field="metadata" align="center" gap="sm">
+            <Flex align="center" gap="xxs">
+              <Icon icon="fa-pro-light:calendar" />
+              {{ item.date }}
+            </Flex>
+            <Flex v-if="author && item.author" align="center" gap="xxs">
+              <Icon icon="fa-pro-light:user-circle" />
+              {{ item.author }}
+            </Flex>
+            <Flex
+              v-if="item.claps"
+              align="center"
+              gap="xxs"
+              data-field="clap"
+              @click="() => clap(item)"
+            >
+              <Icon icon="fa-pro-light:hands-clapping" data-state="default" />
+              <Icon icon="fa-pro-solid:hands-clapping" data-state="hover" />
+              {{ item.claps }}
+            </Flex>
+          </Flex>
+          <div data-field="title">{{ item.title }}</div>
+        </template>
+        <template #footer>
+          <Button
+            label="Les mer"
+            size="small"
+            variant="secondary"
+            icon-right="fa-pro-light:circle-plus"
+            @click="() => onDialog(item)"
+          ></Button>
+        </template>
+      </Card>
+    </Grid>
+    <Dialog
+      ref="dialog"
+      :teleport="false"
+      dialog-id="knips"
+      @close="() => onDialog()"
+    >
+      <template #header v-if="item">
+        <Image :src="item.thumbnail" aspect-ratio="16/9" />
+      </template>
+      <template v-if="item">
+        <Flex data-field="metadata" align="center" gap="sm">
+          <Flex align="center" gap="xxs">
+            <Icon icon="fa-pro-light:calendar" />
+            {{ item.date }}
+          </Flex>
+          <Flex v-if="author && item.author" align="center" gap="xxs">
+            <Icon icon="fa-pro-light:user-circle" />
+            {{ item.author }}
+          </Flex>
+          <Flex
+            v-if="item.claps"
+            align="center"
+            gap="xxs"
+            data-field="clap"
+            @click="() => clap(item)"
+          >
+            <Icon icon="fa-pro-light:hands-clapping" data-state="default" />
+            <Icon icon="fa-pro-solid:hands-clapping" data-state="hover" />
+            {{ item.claps }}
+          </Flex>
+        </Flex>
+        <div data-field="dialog-content">
+          <component
+            v-for="{ component, content, ...rest } in item.content"
+            :is="component"
+            v-bind="rest"
+          >
+            <span v-if="content" v-html="content"></span>
+          </component>
+        </div>
+      </template>
+      <template #footer="{ close }" v-if="item">
+        <Button
+          label="Lukk"
+          icon-right="fa-pro-light:times"
+          data-field="close"
+          @click="close"
+        />
+      </template>
+    </Dialog>
+  </div>
 </template>
 
 <style lang="scss">
+@import url('@kvass/ui/style.css');
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+[data-field='metadata'] {
+  font-size: 0.8em;
+}
+
 .knips-feed {
-  border: none;
+  --k-card-border-radius: var(--border-radius);
+  --knips-feed-color: currentColor;
+  --background: var(--knips-feed-background, var(--background-color));
+  --k-card-border-color: rgba(0, 0, 0, 0.05);
+  --k-dialog-backdrop-background: rgba(0, 0, 0, 0.8);
+
+  &__items {
+    [data-field='metadata'] {
+      margin-bottom: var(--k-ui-spacing-xs);
+    }
+
+    .k-card {
+      background-color: var(--background);
+    }
+
+    .k-card__header {
+      padding: 0;
+    }
+  }
+
+  .k-button--variant-tertiary {
+    color: inherit;
+    border-color: currentColor;
+  }
+
+  [data-field='clap'] {
+    cursor: pointer;
+
+    &:hover {
+      [data-state='default'] {
+        display: none;
+      }
+    }
+
+    &:not(:hover) {
+      [data-state='hover'] {
+        display: none;
+      }
+    }
+  }
+
+  .k-dialog[data-id='knips'] {
+    z-index: 10000;
+    position: relative;
+
+    .k-card {
+      max-width: 65ch;
+      background-color: var(--background);
+    }
+
+    .k-card__content {
+      border-top-left-radius: inherit;
+      border-top-right-radius: inherit;
+      overflow: clip;
+      padding: 0;
+    }
+
+    .k-card__footer {
+      position: sticky;
+      bottom: 0;
+      justify-content: center;
+      padding-top: var(--k-card-spacing, var(--k-ui-spacing-lg));
+
+      background: linear-gradient(
+        to bottom,
+        transparent 0%,
+        var(--background) 20%,
+        var(--background) 100%
+      );
+    }
+
+    [data-field='metadata'] {
+      padding: var(--k-card-spacing, var(--k-ui-spacing-lg));
+      padding-bottom: calc(var(--k-card-spacing, var(--k-ui-spacing-lg)) / 4);
+    }
+
+    [data-field='dialog-content'] {
+      padding-inline: var(--k-card-spacing, var(--k-ui-spacing-lg));
+
+      & > * {
+        &:first-child {
+          margin-top: 0;
+        }
+      }
+    }
+  }
 }
 </style>
