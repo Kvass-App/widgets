9573273cdcabe19ffc2f14f3815ad41ae61ccf52
Lasse Ranum <lasse.ranum@gmail.com>
2025-07-24 09:28:19 +0200

    feat: resimo embed

---
+++ src/resimo/components/Resimo.ce.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  iframe_src: {
+    type: String,
+    required: true,
+  },
+})
+
+console.log()
+console.log()
+console.log()
+
+console.log('lastet inn!')
+console.log()
+console.log()
+</script>
+
+<template>
+  <h2>Resimo</h2>
+  <div class="kvass-resimo__wrapper">
+    <iframe :src="props.iframe_src" frameborder="0" allow="fullscreen"></iframe>
+  </div>
+</template>
+
+<style lang="scss">
+.kvass-resimo {
+  &__wrapper {
+    width: 100%;
+  }
+}
+
+iframe {
+  width: 100%;
+  height: 100vh;
+}
+</style>
