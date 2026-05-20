d2da5a698acfeb81af32e37ca4f4e81d1a4595af
Sander Telle <sander@kvass.no>
2026-05-20 21:52:58 +0200

    fix(validator): add email validation rule

--- src/finn/composeable/Validator.ts
+++ src/finn/composeable/Validator.ts
@@ -4,6 +4,17 @@ import nb from 'validatorjs/src/lang/nb_NO'
 
 ValidatorUtils.setLabels(nb)
 
+const EMAIL_REGEX = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/
+
+ValidatorUtils.register(
+  'no_email',
+  (value: any) => {
+    if (typeof value !== 'string') return true
+    return !EMAIL_REGEX.test(value)
+  },
+  ':attribute kan ikke inneholde e-postadresse',
+)
+
 // import CustomValidators from './CustomValidators'
 // import CustomAsyncValidators from './CustomAsyncValidators'
 
